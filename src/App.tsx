8eee4f2b9624a9b4ccc7e2a2090a61b3dbcd0a9a
Anthony Guilherme <anthonytguilherme@hotmail.com>
2022-09-14 16:51:10 -0300

    Create a new project

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import './styles/global.scss'
+import styles from './App.module.scss';
+
+
+import { Header } from './components/Header'
+import HappyPersonPng from './assets/happy-person.png';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className={styles.container}>
+        <div className={styles.welcome}>
+          <div className={styles.welcomeContent}>
+            <div>
+              <span>Boas vindas a AfroTour</span>
+              <h1>Sua viagem dos sonhos é aqui.</h1>
+              <p>Estamos empenhados em fornecer os melhores planos de viagem do brasil para você.</p>
+              <button> Agende sua viagem</button>
+            </div>
+            <img src={HappyPersonPng} alt="Pessoa muito feliz" />
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.scss'
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div>
+        Afro<span>Tour</span>
+      </div>
+    </header>
+  )
+}
