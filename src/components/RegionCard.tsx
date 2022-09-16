deb4cbeced4d84f3fe64d4fc2a7f58c69749cccc
Anthony Guilherme <anthonytguilherme@hotmail.com>
2022-09-16 14:39:38 -0300

    add a new navigate button

--- src/components/RegionCard.tsx
+++ src/components/RegionCard.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './RegionCard.module.scss';
 
 interface RegionCardProps {
@@ -7,9 +8,14 @@ interface RegionCardProps {
   minPrice: number;
 }
 
-export function RegionCard({ title, img, description, minPrice }) {
+export function RegionCard({ title, img, description, minPrice }: RegionCardProps) {
+  const navigate = useNavigate()
+  
+  function handleNavigation() {
+    navigate('/region')
+  }
   return (
-    <li className={styles.regionCard}>
+    <li className={styles.regionCard} onClick={handleNavigation}>
       <img src={img} />
       <div>
         <h3>{title}</h3>
